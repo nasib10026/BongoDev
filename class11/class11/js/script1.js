5f23d36b8ab8688caf0f3364d11cae26fef35d1c
nasib10026 <nasib1026@gmail.com>
2024-04-23 22:36:56 +0600

    edited

--- class11/class11/js/script1.js
+++ class11/class11/js/script1.js
@@ -1,24 +1,31 @@
-function getButtonElement()
-{
+function getButtonElement() {
     const button = document.getElementById('btn');
     return button;
 }
 
-function updateButtontext(buttonText)
-{
-// const button = getButtonElement();
-// button.innerText = buttonText;
-getButtonElement().innerText = buttonText;
+function updateButtontext(buttonText) {
+    // const button = getButtonElement();
+    // button.innerText = buttonText;
+    getButtonElement().innerText = buttonText;
 }
 const buttonText = "click me";
 updateButtontext(buttonText);
 // updateButtontext('Simple Button');
+let a = 1;
 
-function updateButtonTextWithClick()
-{
+function updateButtonTextWithClick() {
     console.log("button is clicked");
-    updateButtontext('Simple Button');
+    if (a == 1) {
+        updateButtontext('Simple Button');
+        a = 0;
+    }
+    else
+    {
+        updateButtontext('Click me');
+        a = 1; 
+    }
+
 }
 //const button = document.getElementById('btn');
 
-getButtonElement().addEventListener('click',updateButtonTextWithClick);
+getButtonElement().addEventListener('click', updateButtonTextWithClick);
