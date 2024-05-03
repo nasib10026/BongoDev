d2af5c32b51844d6eb66463ae76357000eb7ebae
nasib10026 <nasib1026@gmail.com>
2024-05-04 02:54:44 +0600

    hw modified

--- class14/index.js
+++ class14/index.js
@@ -1,36 +1,32 @@
 //const numberInput = document.getElementById('number');
 const btn = document.getElementById('submit');
-const table_container1 = document.getElementById('table-container1');
-const table_container2 = document.getElementById('table-container2');
 const reset = document.getElementById('reset');
-
+let k = 1;
 btn.addEventListener('click', function () {
-    table_container1.innerHTML = '';
-    table_container2.innerHTML = '';
     for (let i = 1; i <= 5; i++) {
+        document.getElementById(`table-container${i}`).innerHTML = '';
+    }
+    for (let i = 1; i <= 10; i++) {
         for (let j = 1; j <= 10; j++) {
             const tableRow = document.createElement('h4');
             tableRow.innerText = `${i} X ${j} = ${i*j}`;
-            table_container1.appendChild(tableRow);
+            document.getElementById(`table-container${k}`).appendChild(tableRow);
+
         }
         const lineBreak = document.createElement('br');
-        table_container1.appendChild(lineBreak);
-    }
-
-    for (let i = 6; i <= 10; i++) {
-        for (let j = 1; j <= 10; j++) {
-            const tableRow = document.createElement('h4');
-            tableRow.innerText = `${i} X ${j} = ${i*j}`;
-            table_container2.appendChild(tableRow);
+        document.getElementById(`table-container${k}`).appendChild(lineBreak);
+        if (i % 2 == 0) {
+            k++;
         }
-        const lineBreak = document.createElement('br');
-        table_container2.appendChild(lineBreak);
+        if (k == 6) {
+            return;
+        }
+    }
+
+})
+
+reset.addEventListener('click', function () {
+    for (let i = 1; i <= 5; i++) {
+        document.getElementById(`table-container${i}`).innerHTML = '';
     }
 })
-
-reset.addEventListener('click',function()
-{
-    table_container1.innerHTML = '';
-    table_container2.innerHTML = '';
-})
-
