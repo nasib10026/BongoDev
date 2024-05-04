950efcabcfde58be7aebde4bc46c48714fae10f2
nasib10026 <nasib1026@gmail.com>
2024-05-04 14:07:36 +0600

    new method added

--- class14/index2.js
+++ class14/index2.js
@@ -3,37 +3,36 @@ const reset = document.getElementById('reset');
 const displayResult = document.getElementById('display-result');
 const resultHeadline = document.getElementById('resultHeadline');
 
-
 btn.addEventListener('click', function () {
     resultHeadline.innerText = 'Here is your multiplication table chart';
     const colno = document.getElementById('colno');
     displayResult.innerHTML = '';
     const range1 = parseInt(document.getElementById('range1').value);
     const range2 = parseInt(document.getElementById('range2').value);
-    const range = range2-range1+1;
-    for (let i = 1; i <= range ; i++) {
-        const tableContainer = document.createElement('div');
-        tableContainer.id = `table-container${i}`;
-        tableContainer.className = 'mt-4';
-        displayResult.appendChild(tableContainer);
-    }
+    const columns = colno.value; 
 
-    let k = 1;
+    displayResult.style.display = 'grid';
+    displayResult.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    displayResult.style.gap = '50px'; 
+
     for (let i = range1; i <= range2; i++) {
+        const tableContainer = document.createElement('div');
+        tableContainer.className = 'mt-2 mb-2';
+
         for (let j = 1; j <= 10; j++) {
             const tableRow = document.createElement('h4');
             tableRow.innerText = `${i} X ${j} = ${i * j}`;
-            document.getElementById(`table-container${k}`).appendChild(tableRow);
+            tableContainer.appendChild(tableRow);
         }
-        const lineBreak = document.createElement('br');
-        document.getElementById(`table-container${k}`).appendChild(lineBreak);
-        if ((i-range1+1) % colno.value == 0) {
-            k++;
-        }
+
+        displayResult.appendChild(tableContainer);
     }
 });
 
 reset.addEventListener('click', function () {
     resultHeadline.innerText = '';
     displayResult.innerHTML = '';
+    displayResult.style.display = '';
+    displayResult.style.gridTemplateColumns = ''; 
+    displayResult.style.gap = ''; 
 });
